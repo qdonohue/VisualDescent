68cf8ab085621c5673e06101555502860312df20
qdonohue <43917220+qdonohue@users.noreply.github.com>
2019-05-24 12:12:04 -0700

    added in helpers to change values of iteration, alpha, loss

--- Scripts/UI.js
+++ Scripts/UI.js
@@ -3,15 +3,15 @@
 // in Jquery and make these functions work
 function updateAlpha(newAlpha) {
     var alphaDisplay = $('#currentAlpha');
-
+    alphaDisplay.html(newAlpha);
 }
 
 function updateLoss(newLoss) {
     var lossDisplay = $('#currentLoss');
-
+    lossDisplay.html(newLoss);
 }
 
 function updateIteration(newIteration) {
     var iterationDisplay = $('currentIteration');
-    
+    iterationDisplay.html(newIteration);
 }
