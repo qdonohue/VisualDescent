d1a833fe30e37e58fc42148c327dfda53854fed8
qdonohue <43917220+qdonohue@users.noreply.github.com>
2019-05-24 00:08:13 -0400

    tired... done for the night and can't remeber if I have work to commit

---
+++ Scripts/UI.js
@@ -0,0 +1,17 @@
+
+// TO DO - figure out how the hell you update innerHTML 
+// in Jquery and make these functions work
+function updateAlpha(newAlpha) {
+    var alphaDisplay = $('#currentAlpha');
+
+}
+
+function updateLoss(newLoss) {
+    var lossDisplay = $('#currentLoss');
+
+}
+
+function updateIteration(newIteration) {
+    var iterationDisplay = $('currentIteration');
+    
+}
